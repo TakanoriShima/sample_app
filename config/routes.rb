1c1f8eff70b492b0469430a0ac9f44ae2b96f8e0
EC2 Default User <ec2-user@ip-172-31-13-100.ap-northeast-1.compute.internal>
2022-09-15 00:37:30 +0000

    root to 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'todolists#index'
   get 'top' => 'homes#top'
   get 'todolists/new'
   post 'todolists' => 'todolists#create'
