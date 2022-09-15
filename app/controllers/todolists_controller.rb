e7e9bdb5ef89faa12b1dfb0b3cae37469191bf84
EC2 Default User <ec2-user@ip-172-31-13-100.ap-northeast-1.compute.internal>
2022-09-15 00:11:38 +0000

    Chapter 5 閲覧機能を作ろう

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,22 +1,21 @@
 class TodolistsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+  def show
+    @list = List.find(params[:id])
+  end
   def new
-    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
     @list = List.new
   end
-  # ---- ここからコードを書きましょう ---- #
   def create
-    # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to todolist_path(list.id)
   end
-
+  
   private
-  # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  # ---- ここまでのあいだにコードを書きましょう ---- #
 end
